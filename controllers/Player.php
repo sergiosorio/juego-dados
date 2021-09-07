49f7e37e80abf2792e16efe524b5c76ca473342c
andresdeveloper91 <sergiosorio_91@hotmail.com>
2021-09-07 03:31:45 -0500

    Se añade el condicional if en el boton denominado lanzar

--- controllers/Player.php
+++ controllers/Player.php
@@ -22,26 +22,28 @@ class Player extends Lanzamiento{
     //Verificar que los inputs tengan los nombres de los jugadores
     public function mensaje()
     {
-        if(empty($_POST['jugador1']) and empty($_POST['jugador2'])){
-            $mensaje = "debe ingresar los nombres de los jugadores";
-        }else{
-            $this->setPlayer1($_POST['jugador1']);
-            if(empty($_POST['jugador2'])){
-                $mensaje = "{$this->getPlayer1()} debes tener un contrincante";
+        if(isset($_POST['lanzar'])){
+            if(empty($_POST['jugador1']) and empty($_POST['jugador2'])){
+                $mensaje = "debe ingresar los nombres de los jugadores";
             }else{
-                $this->setPlayer2($_POST['jugador2']);
-                if(empty($_POST['jugador1'])){
-                    $mensaje = "{$this->getPlayer2()} debes tener un contrincante";
-                }else{   
-                    $this->throw = rand(1,6);
-                    $this->throw1 = rand(1,6);                
-                    $mensaje = "{$this->getPlayer1()} el numero que lanzaste fue {$this->throw}\n<br>
-                    {$this->getPlayer2()} el numero que lanzaste fue {$this->throw1}\n<br/>";
-                    
+                $this->setPlayer1($_POST['jugador1']);
+                if(empty($_POST['jugador2'])){
+                    $mensaje = "{$this->getPlayer1()} debes tener un contrincante";
+                }else{
+                    $this->setPlayer2($_POST['jugador2']);
+                    if(empty($_POST['jugador1'])){
+                        $mensaje = "{$this->getPlayer2()} debes tener un contrincante";
+                    }else{   
+                        $this->throw = rand(1,6);
+                        $this->throw1 = rand(1,6);                
+                        $mensaje = "{$this->getPlayer1()} el numero que lanzaste fue {$this->throw}\n<br>
+                        {$this->getPlayer2()} el numero que lanzaste fue {$this->throw1}\n<br/>";
+                        
+                    }
                 }
             }
+            return $mensaje;
         }
-        return $mensaje;
     }//end method mensaje()
 
     //Identificar que jugador es el ganador
